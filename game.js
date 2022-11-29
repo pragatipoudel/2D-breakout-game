65700539354b33afe227ab7eb41b05719f9664f0
Pragati Poudel <pragati.poudel37@gmail.com>
2022-11-29 14:23:00 -0500

    Complete the game

--- game.js
+++ game.js
@@ -28,6 +28,8 @@ let ballColor = "#0095DD";
 
 let score = 0;
 
+let lives = 3;
+
 const bricks = [];
 for (let c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
@@ -78,7 +80,7 @@ function collisonDetection() {
                     if (score === brickRowCount * brickColumnCount) {
                         alert("YOU WIN, CONGRATULATIONS");
                         document.location.reload();
-                        clearInterval(interval);
+                        
                     }
                    
                 }
@@ -92,6 +94,12 @@ function drawScore() {
     ctx.font = "16px Arial";
     ctx.fillStyle = "#0095DD";
     ctx.fillText(`Score: ${score}`, 8, 20);
+}
+
+function drawLives() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText(`Lives: ${lives}`, canvas.width - 65, 20);
 }
 function drawBall() {
     ctx.beginPath();
@@ -133,6 +141,7 @@ function draw() {
     drawBall();
     drawPaddle();
     drawScore();
+    drawLives();
     collisonDetection();
     x += dx;
     y += dy;
@@ -145,12 +154,24 @@ function draw() {
         dy = -dy;
     } else if (y + dy > canvas.height - ballRadius) {
         if (x > paddleX && x < paddleX + paddleWidth) {
-            dy = -dy
+            dy = -dy;
         } else {
-        alert("GAME OVER");
-        document.location.reload();
-        clearInterval(interval);
+            lives--;
+            if (!lives) {
+                alert("GAME OVER");
+                document.location.reload();
+               
+            } else {
+                x = canvas.width / 2;
+                y = canvas.height - 30;
+                dx = 2;
+                dy = -2;
+                paddleX = (canvas.width - paddleWidth) / 2;
+            }
+
+
         }
+        
     }
 
     if (rightPressed) {
@@ -159,8 +180,8 @@ function draw() {
         paddleX = Math.max(paddleX - 2, 0);
     }
 
-
+    requestAnimationFrame(draw);
 }
 
 
-const interval = setInterval(draw, 10);
+draw();
